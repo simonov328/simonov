1091ef07a71f0139dbc7926d01309b441c1b694c
simonov328 <147929057+simonov328@users.noreply.github.com>
2023-10-21 14:20:09 +0300

    Add files via upload

---
+++ LESSON2/main.py
@@ -0,0 +1,6 @@
+from student import Student
+bohdan = Student("Bohdan Andriienko", 13.0, "C20112")
+print(bohdan)
+
+teacher = ("hello teacher:")
+print(teacher)
---
+++ LESSON2/student.py
@@ -0,0 +1,9 @@
+class Student:
+    def __init__(self, name: str, age: float, group: str):
+        self.Name = name
+        self.Age = float
+        self.Group = group
+    def __str__(self):
+        return (f'Name - {self.Name}\n' 
+                f'Age - {self.Age}\n'   
+                f'Group - {self.Group}')
